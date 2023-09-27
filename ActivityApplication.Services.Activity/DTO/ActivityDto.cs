2166be3fec7ba707ca6a1503993f2e1f90bad56e
ali <ali.vortex799@gmail.com>
2023-09-26 23:46:47 -0400

    Add Id to Dto to send to front side

--- ActivityApplication.Services.Activity/DTO/ActivityDto.cs
+++ ActivityApplication.Services.Activity/DTO/ActivityDto.cs
@@ -2,6 +2,7 @@ namespace ActivityApplication.Services.Activity.DTO;
 
 public record struct ActivityDto
 {
+    public Guid Id { get; set; }
     public string Title { get; set; }
     public DateTime Date { get; set; }
     public string Description { get; set; }
